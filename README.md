d7a8eea1eb31b8e9308156153a16425bbe91ee58
feynman <2901954062@qq.com>
2022-02-14 22:09:34 +0800

    compile plugins ,add serve command

--- README.md
+++ README.md
@@ -1,3 +1,85 @@
+
+
+## 项目初始化运行
+
+
+
+代码地址克隆
+
+`git clone xxxxx`
+
+
+
+安装依赖
+
+`npm i  `
+
+
+
+这里本地需要全局安装gulp
+
+`npm i -g gulp`
+
+
+
+安装依赖也可以使用yarn add
+
+
+
+
+
+## 开发笔记
+
+
+
+1. module.exports导出使用，require 引入使用，遵循node.js 模块化规范
+2. browserSync  server.router 值是文件夹要提供的（相对于当前的工作目录）
+
+```js
+const serve = () => {
+  bs.init({
+    server: {
+      baseDir: 'dist',
+      routes: {
+        '/node_modules': 'node_modules'
+      }
+    }
+  })
+}
+```
+
+
+
+3. 自动加载插件处理 可以使用gulp-load-plugins插件使用时进行加载
+
+   ```js
+   
+   // 热更新
+   const browserSync = require('browser-sync')
+   
+   const plugins = loadPlugins()
+   
+   const style = () => {
+     return src('src/assets/styles/*.scss',{base: 'src'})
+       .pipe(plugins.sass({outputStyle: 'expended'}))
+       .pipe(dest('dist'))
+   }
+   ```
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
 ## git 操作命令
 
 
--- gulpfile.js
+++ gulpfile.js
@@ -2,15 +2,19 @@
  * @Author: pink
  * @Date: 2022-02-08 20:31:06
  * @LastEditors: pink
- * @LastEditTime: 2022-02-14 21:26:18
+ * @LastEditTime: 2022-02-14 21:50:09
  * @Description: 自动构建入口文件
  */
 const {src, dest, parallel, series} = require('gulp')
-const sass = require('gulp-sass')
-const babel = require('gulp-babel')
-const swig = require('gulp-swig')
-const imagemin = require('gulp-imagemin')
+
+//自动加载插件处理
+const loadPlugins = require('gulp-load-plugins')
 const del = require('del')
+// 热更新
+const browserSync = require('browser-sync')
+
+const plugins = loadPlugins()
+const bs = browserSync.create()
 
 
 // html文件中出现的变量对象
@@ -27,41 +31,52 @@ const clean = () => {
 // css样式调整
 const style = () => {
   return src('src/assets/styles/*.scss',{base: 'src'})
-    .pipe(sass({outputStyle: 'expended'}))
+    .pipe(plugins.sass({outputStyle: 'expended'}))
     .pipe(dest('dist'))
 }
 
 // js文件处理
 const script = () => {
   return src('src/assets/scripts/*.js',{base: 'src'})
-    .pipe(babel({presets: ['@babel/preset-env']}))//将js es6写法转化为es5
+    .pipe(plugins.babel({presets: ['@babel/preset-env']}))//将js es6写法转化为es5
     .pipe(dest('dist'))
 }
 
 // 将基础的html文件进行打包
 const page = () => {
   return src('src/*.html', {base: 'src'})
-    .pipe(swig({data}))
+    .pipe(plugins.swig({data}))
     .pipe(dest('dist'))
 }
 
 // 图片资源处理
 const image = () => {
   return src('src/assets/images/**', {base: 'src'})
-    .pipe(imagemin())
+    .pipe(plugins.imagemin())
     .pipe(dest('dist'))
 }
 
 // 项目中文件字体处理
 const font = () => {
   return src('src/assets/fonts/**',{base: 'src'})
-    .pipe(imagemin())
+    .pipe(plugins.imagemin())
     .pipe(dest('dist'))
 }
 
 const extra = () => {
   return src('public/**',{base: 'public'})
     .pipe(dest('dist'))
+}
+
+const serve = () => {
+  bs.init({
+    server: {
+      baseDir: 'dist',
+      routes: {
+        '/node_modules': 'node_modules'
+      }
+    }
+  })
 }
 
 // 总体编译同步流程处理
@@ -71,5 +86,6 @@ const build = series(clean, parallel(compile, extra))
 
 module.exports = {
   compile,
-  build
+  build,
+  serve
 }
